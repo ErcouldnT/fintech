e6511a812bd5696c9ea03583a03080d2e8f9fbe3
Ercode Techoglu <ercode@pm.me>
2024-12-03 01:16:17 +0300

    Update +page.server.ts

--- src/routes/[date]/+page.server.ts
+++ src/routes/[date]/+page.server.ts
@@ -1,5 +1,28 @@
-export function load({ params }) {
+import { getIncomesByDate, getOutgoingsByDate } from '$lib/server/db/queries/select';
+import type { SelectIncome } from '$lib/server/db/schema';
+
+export async function load({ params }) {
+	const date = params.date;
+	const incomes = await getIncomesByDate(date);
+	const outgoings = await getOutgoingsByDate(date);
+
 	return {
-		date: params.date
+		date,
+		incomes,
+		outgoings
 	};
 }
+
+export const actions = {
+	income: async ({ request }) => {
+		const data = await request.formData();
+		console.log(data);
+		return { success: true };
+	},
+
+	outgoing: async ({ request }) => {
+		const data = await request.formData();
+		console.log(data);
+		return { success: true };
+	}
+};
