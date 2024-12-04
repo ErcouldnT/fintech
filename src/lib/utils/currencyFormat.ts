b4110f24d9a79e63aa31327eff52a7fe855b12b4
Ercode Techoglu <ercode@pm.me>
2024-12-04 20:07:29 +0300

    fix: currency string parameter

--- src/lib/utils/currencyFormat.ts
+++ src/lib/utils/currencyFormat.ts
@@ -1,8 +1,12 @@
-export const formatter = (currency: number) => {
+export const formatter = (currency: number | string) => {
 	const formatter = new Intl.NumberFormat('tr-TR', {
 		style: 'currency',
 		currency: 'TRY'
 	});
+
+	if (typeof currency === 'string') {
+		currency = Number(currency);
+	}
 
 	return formatter.format(currency).slice(1) + ' ₺'; // ₺1.000.985,10
 };
