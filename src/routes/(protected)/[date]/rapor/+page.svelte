04e6baa2a6e7003ec5e05e64d95bc647c10d29a4
Ercode Techoglu <ercode@pm.me>
2024-12-21 05:13:35 +0300

    feat: url değişiminde component re-render

--- src/routes/(protected)/[date]/rapor/+page.svelte
+++ src/routes/(protected)/[date]/rapor/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import MonthlyReport from '$lib/components/MonthlyReport.svelte';
 	import { monthAndYear, nextMonth, previousMonth } from '$lib/utils/dateFormat';
 	import { ChevronLeft, ChevronRight } from 'lucide-svelte';
+	import { goto } from '$app/navigation';
 
 	let { data } = $props();
+
+	const handleNavigation = async (url: string) => {
+		await goto(url);
+	};
 </script>
 
 <div class="text-center">
@@ -14,23 +20,28 @@
 <hr class="hr border-t-2" />
 
 <div class="flex justify-between gap-4 text-sm">
-	<a
-		href={'/' + previousMonth(data.date) + '/rapor'}
-		data-sveltekit-reload
+	<button
 		type="button"
 		class="btn"
+		onclick={() => handleNavigation('/' + previousMonth(data.date) + '/rapor')}
 	>
 		<ChevronLeft />
 		<span>Önceki ay</span>
-	</a>
+	</button>
 
-	<a href={'/' + nextMonth(data.date) + '/rapor'} data-sveltekit-reload type="button" class="btn">
+	<button
+		type="button"
+		class="btn"
+		onclick={() => handleNavigation('/' + nextMonth(data.date) + '/rapor')}
+	>
 		<span>Sonraki ay</span>
 		<ChevronRight />
-	</a>
+	</button>
 </div>
 
-<MonthlyReport incomes={data.monthlyIncomes} outgoings={data.monthlyOutgoings} />
+{#key $page.url.pathname}
+	<MonthlyReport incomes={data.monthlyIncomes} outgoings={data.monthlyOutgoings} />
+{/key}
 
 <div class="text-center text-sm text-success-400">
 	<a data-sveltekit-reload href="/">Ana Sayfaya Git</a>
